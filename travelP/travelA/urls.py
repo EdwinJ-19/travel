b15dc114fbac3848f81c88366348ccb9f38ee7ad
EdwinJ-19 <edwinjames2001@gmail.com>
2023-12-29 12:22:11 +0530

    about_html_css_only

--- travelP/travelA/urls.py
+++ travelP/travelA/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.payment),
+    path('',views.team),
     # path('hotelacc/',views.hotel),
     # path('index/',views.login),
     # path('team/',views.team),
